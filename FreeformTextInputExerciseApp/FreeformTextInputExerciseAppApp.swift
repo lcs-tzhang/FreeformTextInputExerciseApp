ac16dc11bc0499bc42352619296793608c896136
Tiffany Zhang <tzhang@lcs.on.ca>
2025-02-21 14:03:31 -0500

    Initial Commit

---
+++ FreeformTextInputExerciseApp/FreeformTextInputExerciseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeformTextInputExerciseAppApp.swift
+//  FreeformTextInputExerciseApp
+//
+//  Created by xinyu zhang on 2025-02-21.
+//
+
+import SwiftUI
+
+@main
+struct FreeformTextInputExerciseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
